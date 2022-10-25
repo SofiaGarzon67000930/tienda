3b18aa59adef77a7e64197a645f9af33d15f23b9
Araceli <Araceli@DESKTOP-F2I8P92>
2022-10-25 00:37:17 -0500

    Ejercicios tienda SeLeTiene.com

---
+++ src/main/java/com/mycompany/tienda/Producto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tienda;
+
+/**
+ *
+ * @author Araceli
+ */
+public abstract class Producto  implements Unidad,Bolsa{
+     String gama;
+     int precio;
+}
